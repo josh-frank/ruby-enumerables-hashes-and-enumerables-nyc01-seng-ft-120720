63184c9a32f540cc7ddbcdb0d55a0ec02fd67bb7
Joshua Frank <eurosewusa@gmail.com>
2020-10-28 22:32:54 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,8 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = String.new
+  passengers.each do | suite, name |
+    winner = name if suite == :suite_a && name.start_with?( "A" )
+  end
 end
